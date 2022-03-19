2343b1b5b5beed4f8f8d9d6e8622da5b45d38a3f
Ewig <valikallstars@gmail.com>
2022-03-19 18:41:33 +0200

    added modals for calculator

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,35 +1,52 @@
 const modals = () => {
-	function bindModal(triggerSelector, modalSelector, closeSelector) {
+	function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 
 		const trigger = document.querySelectorAll(triggerSelector),
 			modal = document.querySelector(modalSelector),
-			close = document.querySelector(closeSelector);
+			close = document.querySelector(closeSelector),
+			windows = document.querySelectorAll('[data-modal]');
 
-
+		// кнопка тригер
 		trigger.forEach(item => {
 			item.addEventListener('click', (e) => {
 				if (e.target) {
 					e.preventDefault()
 				}
 
+				windows.forEach(item => {
+					item.style.display = 'none';
+				});
+
 				modal.style.display = 'block';
 				document.body.style.overflow = 'hidden';
 			});
 		})
 
+		// кнопка закрытия
 		close.addEventListener('click', () => {
+			windows.forEach(item => {
+				item.style.display = 'none';
+			});
+
 			modal.style.display = 'none';
 			document.body.style.overflow = '';
+
 		});
 
+		// подложка
 		modal.addEventListener('click', (e) => {
-			if (e.target === modal) {
+			if (e.target === modal && closeClickOverlay) {
+				windows.forEach(item => {
+					item.style.display = 'none';
+				});
+
 				modal.style.display = 'none';
 				document.body.style.overflow = '';
 			}
 		})
 	}
 
+	// Таймер запуска
 	function showModalByTime(selector, time) {
 		setTimeout(function () {
 			document.querySelector(selector).style.display = 'block';
@@ -40,6 +57,12 @@ const modals = () => {
 	/* 	showModalByTime('.popup', 60000) */
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
+	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc .popup_calc_close');
+	bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile .popup_calc_profile_close', false);
+	bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end .popup_calc_end_close', false);
 };
 
 export default modals;
+
+//
+// таймер, модальное окно появляется через 60сек
